32eabf613c2956390042b13e5572767a184bfcb5
naopen <naokizssm@gmail.com>
2025-03-22 23:21:23 +0900

    Ex00 ClapTrap: Change access modifier from protected to private and remove virtual specifier from destructor

--- Ex00/ClapTrap.hpp
+++ Ex00/ClapTrap.hpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 class ClapTrap {
-protected:  // privateではなくprotected (Exercise 01以降で継承するため)
+private:
     std::string _name;
     unsigned int _hitPoints;
     unsigned int _energyPoints;
@@ -16,9 +16,9 @@ public:
     ClapTrap(const std::string& name);
     ClapTrap(const ClapTrap& other);
     ClapTrap& operator=(const ClapTrap& other);
-    virtual ~ClapTrap();  // 派生クラスでoverrideするためvirtualに
+    ~ClapTrap();
 
-    virtual void attack(const std::string& target);
+    void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
 
